06a4b142294de7f6298d95e3788204ca871ae40b
kimhanui <cocacola2017@naver.com>
2021-12-15 09:59:01 +0900

    chore : Initial Project Setting

---
+++ src/main/java/com/smilegate/loginsg/LoginSgApplication.java
@@ -0,0 +1,13 @@
+package com.smilegate.loginsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSgApplication.class, args);
+    }
+
+}
